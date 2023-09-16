e957dce40a9a0746f419c924f9dc0a48667a2fb7
georgeyoumansjr <georgeyoumansjr@gmail.com>
2023-09-16 11:56:52 -0400

    initial commit

---
+++ worker/celery.py
@@ -0,0 +1,18 @@
+
+# celery.py
+
+import os
+from celery import Celery
+from django.conf import settings
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'worker.settings')
+
+app = Celery('worker')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
